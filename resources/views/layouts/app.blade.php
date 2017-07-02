abf0dd2fba3f9ae34a108d5415690346573139fe
Anton Kolesov <Anton Kolesov>
2017-07-03 00:57:07 +0500

    стили CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Вопросы и ответы</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
